a15bf0764c14030878428db493297cb5c3bc72ea
robnez44 <00044623@uca.edu.sv>
2024-06-08 19:46:50 -0600

    Creando repositorio

---
+++ ICPC problems/ICPC 08-06-2024/L.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N;
+    vector<int> a(N);
+    vector<int> b(N);
+
+    while (cin >> N)
+    {
+        for (int i = 0; i < ; ++i)
+        {
+            
+        }
+        
+    }
+    
+
+
+    return 0;
+}
---
+++ ICPC problems/Trainings/Mia.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int s0, s1, r0, r1, highest;
+   
+    
+
+    return 0;
+}
