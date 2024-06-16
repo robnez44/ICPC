8559b1ce0f109de1660177699db362f0a2a45479
robnez44 <00044623@uca.edu.sv>
2024-06-16 00:20:29 -0600

    Rightsizing.cpp done with Time limit exceeded

--- ICPC problems/ICPC 04-05-2024/Rightsizing.cpp
+++ ICPC problems/ICPC 04-05-2024/Rightsizing.cpp
@@ -1,11 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a, n;
-    long int s, r;
-    string e;
+int main()
+{
 
+    int e, a, action;
+    map<string, long int> emp;
+    string name;
+    long int salary, raise;
+
+    cin >> e >> a;
+
+    for (int i = 0; i < e; ++i)
+    {
+        cin >> name >> salary;
+        emp[name] = salary;
+    }
+
+    for (int i = 0; i < a; ++i)
+    {
+        cin >> action;
+
+        if (action == 1)
+        {
+            cin >> name >> raise;
+            if (emp.find(name) != emp.end()) emp[name] += raise;
+        }
+        else
+        {
+            auto it = emp.begin();
+            long int maxSalary = it->second;
+            
+            while (it != emp.end())
+            {
+                if (it->second > maxSalary) maxSalary = it->second;
+                it++;
+            }
+            
+            for (auto i = emp.begin(); i != emp.end(); ++i)
+            {
+                if (maxSalary == i->second)
+                {
+                    cout << i->first << " " << i->second << "\n";
+                    emp.erase(i);
+                    break;
+                }   
+            }
+        }
+    }
 
     return 0;
 }
