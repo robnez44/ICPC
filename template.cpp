51e41ca1d676e57c9169f9faa1a2e4c24a4ba601
robnez44 <00044623@uca.edu.sv>
2024-07-14 23:15:27 -0600

    nosE

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i, a, b) for(int i = (int) a; i < (int) b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define dforr(i, a, b) for(int i = (int) b-1; i >= (int) a; i--)
+#define dforn(i, n) dforr(i, 0, n)
+#define sz(x) ((int) x.size())
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+const int MAXN = -1;
+
+int n;
+
+int main() {
+	//~ freopen("input.txt", "r", stdin);
+	int tc; scanf("%d", &tc);
+	forn(c s, tc) {
+		scanf("%d", &n);
+	}
+}
